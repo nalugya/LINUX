60ac95d87946eddfdfba5e1b274a46b14a08ebed
nalugya <108033919+nalugya@users.noreply.github.com>
2024-09-03 17:31:59 +0300

    Add files via upload

---
+++ homework4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####################################
+# Name: <Your name here>
+# CSE 391 - Spring 2022
+# Homework 4
+####################################
+
+function print_link {
+  echo "Link to your repo here"
+}
+
+function problem1 {
+  # Type your answer to problem #1 below this line
+}
+
+function problem2 {
+  # Type your answer to problem #1 below this line
+}
+
+function problem3 {
+  # Type your answer to problem #1 below this line
+}
