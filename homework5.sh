01f42db8c6a3ebb288e141d7f7baecbc6d0f96f8
nalugya <108033919+nalugya@users.noreply.github.com>
2024-09-04 15:33:54 +0300

    Add files via upload

---
+++ homework5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+####################################
+# Name: <Your Name Here>
+# BSE 4105 - August 2024
+# Homework 5
+####################################
+
+function print_username {
+  echo "your gitlab username here"
+}
